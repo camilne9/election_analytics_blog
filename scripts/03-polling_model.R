d9d658d6e18f50f46ed09238506ae0a7af681071
= <=>
2020-09-27 19:16:27 -0400

    Completed national and state level poll accuracy over time.

--- scripts/03-polling_model.R
+++ scripts/03-polling_model.R
@@ -29,6 +29,8 @@ cleaned_nat <- national_polls %>%
   mutate(rep2p = 100*rep_support/(dem_support + rep_support)) %>% 
   select(-dem_support, -rep_support)
 
+#Since we are concerned with the accuracy of the polls, we can compare the predicted
+# republican vote share versus the ultimate republican vote share for that year
 nat_accuracy <- pop_vote %>% 
   filter(winner) %>% 
   select(year, party, pv2p) %>% 
@@ -38,9 +40,12 @@ nat_accuracy <- pop_vote %>%
   right_join(cleaned_nat, by = 'year') %>% 
   mutate(rep_diff = rep2p - rep_pv2p)
 
+#Here we plot the difference in the prediction and the final republican vote share
 nat_accuracy %>% 
   ggplot(aes(x = month, y = rep_diff))+
   geom_point()
+
+# Now lets look at the accuracy of state polling
 
 # Now we will use state-level data and map it onto electoral votes. First we look at polled
 # support rates
@@ -85,15 +90,38 @@ previous_winner <- pop_vote_state %>%
   
 
 electors <- cleaned_state %>% 
-  left_join(previous_winner, by = c('year', 'state'))
   right_join(electoral_college_mod, by = c('year', 'state', 'month')) %>% 
+  arrange(year, state, month) %>% 
+  left_join(previous_winner, by = c('year', 'state')) %>% 
   mutate(rep_elector = electors*(rep_support > dem_support)) %>% 
-  group_by(state, year) %>% 
-  fill(rep_elector, .direction = 'down')
+  group_by(year, state) %>%
+  filter(!is.na(winner)) %>%
+  mutate(rep_elector = ifelse(month==0, electors*str_detect(winner, "republican"), rep_elector)) %>% 
+  fill(rep_elector, .direction = 'down') %>% 
+  ungroup() %>% 
+  filter(year >= 1972) %>% 
+  group_by(year, month) %>% 
+  summarize(republican_electoral_votes = sum(rep_elector))
+
+# here we find the number of electoral votes earned by republicans in each election
+state_accuracy <- electoral_college %>% 
+  left_join(pop_vote_state, by = c('state', 'year')) %>% 
+  mutate(rep_win = (R>D)) %>% 
+  filter(rep_win) %>% 
+  group_by(year) %>% 
+  summarize(actual_republican_votes = sum(electors))
+  
+# Now we can plot the difference between the predicted and 
+state_accuracy %>% 
+  left_join(electors, by = c('year')) %>% 
+  mutate(electoral_diff = 100*(republican_electoral_votes/538 - actual_republican_votes/538)) %>% 
+  ggplot(aes(x = month, y = electoral_diff))+
+  geom_point()
+
 
 cleaned_nat %>% 
  # filter(year == 2012) %>% 
   group_by(year) %>% 
-  ggplot(aes(x = month, y = 538*rep2p, color = year))+
+  ggplot(aes(x = month, y = 538*rep2p))+
   geom_point()
   
