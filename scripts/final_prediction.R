e0f18766145f0c43383a5a82bd576cd8b6ac0d59
= <=>
2020-10-29 22:48:57 -0400

    scripts for drafting and finalizing final prediction code

---
+++ scripts/final_prediction.R
@@ -0,0 +1,3 @@
+library(tidyverse)
+
+# In this script I generate all of the relevant content for my final prediction blog post.
---
+++ scripts/prediction_draft.R
@@ -0,0 +1,6 @@
+library(tidyverse)
+
+# In this script I will write code in an effort to sort out the content of my final
+# predictions. This script tends to be disorganized and unclear in its scheme. To see the
+# code that generates everything for the final prediction blog post, please see 
+# final_prediction.R (which is also in the scripts folder).
