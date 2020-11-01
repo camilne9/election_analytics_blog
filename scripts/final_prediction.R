0e24b2508c04fdec877b30974adf9508676dc5fc
= <=>
2020-11-01 18:11:51 -0500

    Fixed table of state predictions

--- scripts/final_prediction.R
+++ scripts/final_prediction.R
@@ -154,31 +154,15 @@ plot_usmap(data = final_prediction, regions = "states", values = "trump_predicte
 
 ggsave("../figures/polling_state_predictions.png", height = 6, width = 8)
 
-# To consider the uncertainty, I generate the standard error in different clusters along the 
-# regression line
-standard_errors <- polls_vs_vote_V2 %>% 
-  mutate(prediction = 1.03279*normalize_rep -1.13674) %>% 
-  mutate(error = prediction - normalize_rep) %>%
-  mutate(stratification = 5*floor((normalize_rep + 5/2)/5)) %>%
-  group_by(stratification) %>% 
-  summarize(stdev = (sum(error**2)/(n()-1))**(.5), count = n()) %>% 
-  mutate(standard_error = stdev/(count)**(.5)) %>%
-  mutate(standard_error = ifelse(stratification == 10, 0.95591132, standard_error)) %>% 
-  filter(standard_error != Inf) %>% 
-  select(stratification, standard_error)
 
-# I merge the standard errors with the tibble with current polls and predictions:
+# I make a table
 final_prediction %>% 
-  mutate(stratification = 5*floor((trump + 5/2)/5)) %>%
-  left_join(standard_errors, by = 'stratification') %>% 
   mutate(State = state, 
-         "Trump Vote Share" = prediction, 
-         "Biden Vote Share" = 100 - prediction,
-         Margin = round(2*standard_error, digits = 2)) %>%
-  mutate(Margin = paste("±", Margin)) %>% 
-  select(State, "Trump Vote Share", "Biden Vote Share", Margin) %>% 
+         "Trump Vote Share" = round(prediction, 1),
+         "Biden Vote Share" = round(100 - prediction, 1)) %>%
+  select(State, "Trump Vote Share", "Biden Vote Share") %>% 
   gt() %>% 
-  tab_header("Predicted State-Level Two-Party Vote Shares") %>% 
+  tab_header("Predicted State-Level Two-Party Vote Shares") %>%
 
 gtsave("../figures/table_state_predictions.png")
 
