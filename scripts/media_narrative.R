1d70cf4c28b590c86cb5d430d54144d979248d6d
= <=>
2020-12-10 11:56:47 -0500

    cleaned up and saved figures

--- scripts/media_narrative.R
+++ scripts/media_narrative.R
@@ -74,7 +74,8 @@ results_demog <- county_demog %>%
 results_demog %>% 
   filter(total < total_vote)
 # I see one county that violates this, but since the census data is not as 
-# recent as the 
+# recent as the election results and the margin is small, this is not
+# concerning.
 
 # I see which candidate wins in each state as another check
 check_results <- results_demog %>% 
@@ -96,13 +97,14 @@ plot_usmap(data = check_results, regions = "states", values = "trump_win") +
 # the validity of the data I do have and I have enough data to test the 
 # relevant media narrative.
 
+ggsave("../figures/media_narrative_figures/verifying_data.png", height = 6, width = 8)
+
+
+# The following two plots are for the state of Arizona. 
+# First I show the percent of the county that is Hispanic.
 plot_usmap(data = results_demog,
              regions = "counties", values = "Hispanic",
-           include = c("AZ")
-             # c(4001,4003,4005,4007,4009,4011,
-             #           4012,4013,4015,4017,4019,4021,4023,
-             #           4025,4027)
-           ) + 
+           include = c("AZ")) + 
   scale_fill_gradient2(
     name = "Percentage \n Hispanic"
   ) +
@@ -110,13 +112,15 @@ plot_usmap(data = results_demog,
   labs(title = "Percent Hispanic by County",
        subtitle = "In Arizona")
 
+ggsave("../figures/media_narrative_figures/az_demog.png", height = 6, width = 8)
+
+# Second I show the change in Democratic Margin between 2016 and 2020.
+# This allows me to see qualitatively whether there is a correlation between
+# Hispanic voters and the win margin.
 plot_usmap(data = results_demog %>% 
              mutate(change = D_margin_2020-D_margin_2016),
            regions = "counties", values = "change",
            include = c("AZ")
-           # c(4001,4003,4005,4007,4009,4011,
-           #           4012,4013,4015,4017,4019,4021,4023,
-           #           4025,4027)
 ) + 
   scale_fill_gradient2(
     name = "Change in \n Margin"
@@ -125,26 +129,14 @@ plot_usmap(data = results_demog %>%
   labs(title = "Change in Democratic Margin by County",
        subtitle = "In Arizona")
 
+ggsave("../figures/media_narrative_figures/az_margin.png", height = 6, width = 8)
 
-# I plot the results.
-plot_usmap(data = check_results, regions = "states", values = "trump_win") + 
-  scale_fill_manual(values = c("blue", "red"), name = "state winner") +
-  theme_void()+
-  theme(legend.position = 'None')+
-  labs(title = "State Map from County Data",
-       subtitle = "",
-       caption = "(Alaska County Data Unavailable)")
-# I do not have Alaska county data, but all other states match the actual
-# result, and the lack of Alaska data does not dwindle my confidence in 
-# the validity of the data I do have and I have enough data to test the 
-# relevant media narrative.
 
+# Now I repeat the two former maps, but in Florida specifically.
+# Here is the visualization of the demographics.
 plot_usmap(data = results_demog,
            regions = "counties", values = "Hispanic",
            include = c("FL")
-           # c(4001,4003,4005,4007,4009,4011,
-           #           4012,4013,4015,4017,4019,4021,4023,
-           #           4025,4027)
 ) + 
   scale_fill_gradient2(
     name = "Percentage \n Hispanic"
@@ -153,13 +145,13 @@ plot_usmap(data = results_demog,
   labs(title = "Percent Hispanic by County",
        subtitle = "In Florida")
 
+ggsave("../figures/media_narrative_figures/fl_demog.png", height = 6, width = 8)
+
+# Here is the visualization of the change in win margin.
 plot_usmap(data = results_demog %>% 
              mutate(change = D_margin_2020-D_margin_2016),
            regions = "counties", values = "change",
            include = c("FL")
-           # c(4001,4003,4005,4007,4009,4011,
-           #           4012,4013,4015,4017,4019,4021,4023,
-           #           4025,4027)
 ) + 
   scale_fill_gradient2(
     name = "Change in \n Margin"
@@ -168,42 +160,118 @@ plot_usmap(data = results_demog %>%
   labs(title = "Change in Democratic Margin by County",
        subtitle = "In Florida")
 
+ggsave("../figures/media_narrative_figures/fl_margin.png", height = 6, width = 8)
+
+# Finally, I repeat the two former maps, but in Texas.
+# Here is the visualization of the demographics.
+plot_usmap(data = results_demog,
+           regions = "counties", values = "Hispanic",
+           include = c("TX")
+) + 
+  scale_fill_gradient2(
+    name = "Percentage \n Hispanic"
+  ) +
+  theme_void()+
+  labs(title = "Percent Hispanic by County",
+       subtitle = "In Texas")
+
+ggsave("../figures/media_narrative_figures/tx_demog.png", height = 6, width = 8)
+
+# Here is the visualization of the change in win margin.
+plot_usmap(data = results_demog %>% 
+             mutate(change = D_margin_2020-D_margin_2016),
+           regions = "counties", values = "change",
+           include = c("TX")
+) + 
+  scale_fill_gradient2(
+    name = "Change in \n Margin"
+  ) +
+  theme_void()+
+  labs(title = "Change in Democratic Margin by County",
+       subtitle = "In Texas")
+
+ggsave("../figures/media_narrative_figures/tx_margin.png", height = 6, width = 8)
+
+
+# I show the Hispanic Population percentage vs the change in margin for all counties for 
+# which I have data and I fit a regression.
 results_demog %>% 
   mutate(change = D_margin_2020-D_margin_2016) %>% 
-  ggplot(aes(x = Hispanic, y = change, color = state))+
+  ggplot(aes(x = Hispanic, y = change))+
   geom_point()+
-  theme(legend.position = 'None')
+  geom_smooth(method = "glm")+
+  theme(legend.position = 'None')+
+  theme_minimal()+
+  labs(title = "Change in Democratic Margin by Hispanic Population Percentage",
+       subtitle = "Each Point Represents a County",
+       x = "Percent of Population that is Hispanic",
+       y = "Change in Democratic Margin")
 
+ggsave("../figures/media_narrative_figures/national_hvm.png", height = 6, width = 8)
+
+# I show the trend state by state instead of all at once. This plot is hard to read
+# so I will isolate some key states.
 results_demog %>% 
   mutate(change = D_margin_2020-D_margin_2016) %>% 
   ggplot(aes(x = Hispanic, y = change))+
   geom_point()+
   labs(title = "Change in Democratic Margin \n by Percentage Hispanic",
        subtitle = "State by State")+
+  geom_smooth(method = "glm")+
   theme(legend.position = 'None')+
   facet_wrap(~state)+
   theme_minimal()
 
+ggsave("../figures/media_narrative_figures/states_hvm.png", height = 6, width = 8)
+
 # I consider which states have the largest Hispanic populations.
 demographics %>% 
   filter(year == 2016) %>%
-  select(state, Hispanic) %>% 
+  mutate(State = state, 
+         "Hispanic Population (%)" = round(Hispanic, 2)) %>% 
   arrange(desc(Hispanic)) %>% 
-  head(10) %>% 
-  gt()
+  select(State, "Hispanic Population (%)") %>% 
+  head(6) %>% 
+  gt() %>% 
+  tab_header("States with Largest Hispanic Populations") %>% 
 
+gtsave("../figures/media_narrative_figures/hispanic_population.png")
+
+# I check the trends in states with large Hispanic populations because
+# they are most likely to be pivotal.
 results_demog %>% 
   mutate(State = state) %>% 
   filter(State %in% c("AZ", "FL", "TX", "NV", "NM", "CA")) %>%
   mutate(change = D_margin_2020-D_margin_2016) %>% 
   ggplot(aes(x = Hispanic, y = change, color = State))+
+  geom_smooth(method = "glm")+
   geom_point()+
   labs(title = "Change in Democratic Margin \n by Percentage Hispanic",
-       subtitle = "State by State",
+       subtitle = "In States with the Largest Hispanic Populations by Percentage",
        x = "Percent of Population that is Hispanic",
        y = "Change in Democratic Margin")+
   facet_wrap(~state)+
   theme_minimal()+
   theme(legend.position = 'None')
 
+ggsave("../figures/media_narrative_figures/populous_hvm.png", height = 6, width = 8)
+
+# I also check the states with small margins of victory because a sinlge demographic
+# could be pivotal in those states.
+results_demog %>% 
+  mutate(State = state) %>% 
+  filter(State %in% c("NC", "GA", "PA", "WI")) %>%
+  mutate(change = D_margin_2020-D_margin_2016) %>% 
+  ggplot(aes(x = Hispanic, y = change, color = State))+
+  geom_smooth(method = "glm")+
+  geom_point()+
+  labs(title = "Change in Democratic Margin \n by Percentage Hispanic",
+       subtitle = "In States with Small Win Margins",
+       x = "Percent of Population that is Hispanic",
+       y = "Change in Democratic Margin")+
+  facet_wrap(~state)+
+  theme_minimal()+
+  theme(legend.position = 'None')
+
+ggsave("../figures/media_narrative_figures/close_hvm.png", height = 6, width = 8)
  
